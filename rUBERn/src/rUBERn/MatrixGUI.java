b6603c5d74b19d8953e16a4331af1b59a44c3a23
frank-117 <franco.velardez@ing.austral.edu.ar>
2016-10-13 13:42:39 -0300

    prueba boton

--- rUBERn/src/rUBERn/MatrixGUI.java
+++ rUBERn/src/rUBERn/MatrixGUI.java
@@ -54,7 +54,7 @@ private Matrix theMatrix;
 	 */
 	private void initialize() {
 		Main = new JFrame();
-		Main.setIconImage(Toolkit.getDefaultToolkit().getImage(MatrixGUI.class.getResource("/Uber-icon2.png")));
+		Main.setIconImage(Toolkit.getDefaultToolkit().getImage(MatrixGUI.class.getResource("Uber-icon2.png")));
 		Main.setTitle("rUBERn - Grupo 3");
 		Main.setBounds(100, 100, 450, 300);
 		Main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
---
+++ rUBERn/src/rUBERn/app.java
@@ -0,0 +1,11 @@
+package rUBERn;
+
+import javax.swing.*;
+
+/**
+ * Created by admin on 13/10/2016.
+ */
+public class app {
+    private JButton button1;
+    private JPanel panel1;
+}
