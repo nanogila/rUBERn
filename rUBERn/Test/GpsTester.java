4719e2cb4df5d2dbf6e295ec86206f87d0a89ea5
Tomascvarela <tomas.varela@ing.austral.edu.ar>
2016-10-15 00:40:21 +0800

    Added more Tests

---
+++ rUBERn/Test/GpsTester.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import rUBERn.*;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by AlumnosFI on 15/10/2016.
+ */
+public class GpsTester {
+    @Test
+    public void locationTester(){
+        Gps prueba = new Gps(10,20);
+        Gps prueba2 = new Gps(20,10);
+        long[] location = {10,20};
+        long[] location2 = prueba.getLocation();
+        assertEquals(location[0],location2[0] );
+        assertEquals(location[1],location2[1] );
+
+
+    }
+    @Test
+    public void distanceTester(){
+        Gps prueba = new Gps(10,20);
+        Gps prueba2 = new Gps(20,10);
+        long[] location = {10,20};
+        double distance = prueba.getDistance(prueba2.getLocation());
+        assertEquals(14.14 , distance , 0.1);
+    }
+}
