02e0600746573bc58a77d2fee48b54a3207bac38
nanogila <nanogi69@gmail.com>
2016-10-27 13:56:54 -0300

    fixed the rest of the tests

--- rUBERn/test/AccountantTester.java
+++ rUBERn/test/AccountantTester.java
@@ -17,16 +17,13 @@ public class AccountantTester {
 	}
 	@Test
 	public void tripCostTest() {
-		ClientBase aBase= new ClientBase();
-		DriverBase anotherBase = new DriverBase();
-		Matrix aMatrix = new Matrix(aBase, anotherBase);
 		Fiat600 aCar = new Fiat600();
 		User aClient = new User("pablo", 233223, "holaaa");
 		Driver aDriver = new Driver("mario", 23423, "holass", aCar);
 		aDriver.updateLocation(3323, 2323);
 		long[] aDestination = {4500, 4300};
 		Trip aTrip = new Trip(aDriver, aClient, aDestination);
-		Accountant anAccountant = new Accountant(aMatrix);
+		Accountant anAccountant = new Accountant();
 		assertEquals(77.241, anAccountant.tripCost(aTrip), 0.001);
 	}
 
