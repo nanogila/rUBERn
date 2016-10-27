b0128db1e7644c2145c1b43fb79d1ae2edea8f9e
nanogila <nanogi69@gmail.com>
2016-10-27 13:45:18 -0300

    fixed Accountant constructor

--- rUBERn/src/logic/Accountant.java
+++ rUBERn/src/logic/Accountant.java
@@ -1,12 +1,12 @@
 package logic;
 import java.util.*;
 
-import GUI.*;
+
 public class Accountant {
-ArrayList<String> Movimientos;
-Matrix theMatrix;
-public Accountant(Matrix aMatrix) {
-	theMatrix = aMatrix;
+ArrayList<String> movimientos;
+
+public Accountant() {
+movimientos = new ArrayList<>();
 }
 public double imageCost(Trip aTrip) {
 	Appraiser anAppraiser = new Appraiser();
