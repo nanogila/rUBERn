beee0583d07729158fc417b4e99b2a7ca34377f8
nanogila <nanogi69@gmail.com>
2016-10-30 22:44:06 -0300

    reduced this tests code a bit

--- rUBERn/test/MatrixTester.java
+++ rUBERn/test/MatrixTester.java
@@ -11,24 +11,29 @@ import logic.*;
 
 public class MatrixTester {
 	Robot bot;
+	ClientBase aBase = new ClientBase();
+	DriverBase anotherBase = new DriverBase();
+	Fiat600 aCar = new Fiat600();
+	Driver aDriver = new Driver("mario", 23423, "holass", aCar);
+	User aUser = new User("Mario", 1231, "hola");
+	Thread ok = new Thread () {
+		  public void run () {
+			  try{Thread.sleep(50);}catch(InterruptedException e){}
+			  bot.keyPress(KeyEvent.VK_ENTER);
+				try{Thread.sleep(50);}catch(InterruptedException e){}
+				bot.keyRelease(KeyEvent.VK_ENTER);
+		  }
+		};
 	@Test
 	public void addMoneyTest() {
-		ClientBase aBase = new ClientBase();
-		DriverBase anotherBase = new DriverBase();
 		Matrix theMatrix = new Matrix(aBase, anotherBase);
-		Fiat600 aCar = new Fiat600();
-		Driver aDriver = new Driver("mario", 23423, "holass", aCar);
 		theMatrix.addDriver(aDriver);
 		theMatrix.addMoney(aDriver, 10);
 		assertEquals(10, aDriver.getBalance(), 0.00001);
 	}
 	@Test
 	public void removeMoneyTest() {
-		ClientBase aBase = new ClientBase();
-		DriverBase anotherBase = new DriverBase();
 		Matrix theMatrix = new Matrix(aBase, anotherBase);
-		Fiat600 aCar = new Fiat600();
-		Driver aDriver = new Driver("mario", 23423, "holass", aCar);
 		theMatrix.addDriver(aDriver);
 		theMatrix.addMoney(aDriver, 20);
 		theMatrix.removeMoney(aDriver, 10);
@@ -36,87 +41,50 @@ public class MatrixTester {
 	}
 	@Test
 	public void removeMoneyFailTest() {
-		ClientBase aBase = new ClientBase();
-		DriverBase anotherBase = new DriverBase();
 		Matrix theMatrix = new Matrix(aBase, anotherBase);
-		Fiat600 aCar = new Fiat600();
-		Driver aDriver = new Driver("mario", 23423, "holass", aCar);
 		theMatrix.addDriver(aDriver);
 		theMatrix.addMoney(aDriver, 20);
 		theMatrix.removeMoney(aDriver, 30);
-		assertEquals(20, aDriver.getBalance(), 0.00001);
+		assertEquals(20, aDriver.getBalance(), 0.01);
 	}
 	@Test
 	public void getUserTest() {
-		ClientBase aBase = new ClientBase();
-		DriverBase anotherBase = new DriverBase();
 		Matrix theMatrix = new Matrix(aBase, anotherBase);
-		User aUser = new User("Mario", 1231, "hola");
+		
 		theMatrix.addUser(aUser);
 		User anotherUser = theMatrix.getUser(aUser.getName());
 		assertEquals(aUser, anotherUser);
 	}
 	@Test
 	public void checkPasswordTest() {
-		ClientBase aBase = new ClientBase();
-		DriverBase anotherBase = new DriverBase();
 		Matrix theMatrix = new Matrix(aBase, anotherBase);
-		User aUser = new User("Mario", 1231, "hola");
 		theMatrix.addUser(aUser);
 		assertTrue(theMatrix.checkPassword(aUser.getName(), "hola"));
 	}
 	@Test
 	public void checkPasswordFailTest() {
-		ClientBase aBase = new ClientBase();
-		DriverBase anotherBase = new DriverBase();
 		Matrix theMatrix = new Matrix(aBase, anotherBase);
-		User aUser = new User("Mario", 1231, "holaaaaaa");
 		theMatrix.addUser(aUser);
-		assertTrue(!theMatrix.checkPassword(aUser.getName(), "hola"));
+		assertTrue(!theMatrix.checkPassword(aUser.getName(), "holal"));
 	}
 	@Test
 	public void checkDriverPasswordFailTest() {
-		ClientBase aBase = new ClientBase();
-		DriverBase anotherBase = new DriverBase();
 		Matrix theMatrix = new Matrix(aBase, anotherBase);
-		Fiat600 aCar = new Fiat600();
-		Driver aDriver = new Driver("mario", 23423, "holass", aCar);
 		theMatrix.addDriver(aDriver);
 		assertTrue(!theMatrix.checkPassword(aDriver.getName(), "hola"));
 	}
 	@Test
 	public void checkDriverPasswordTest() {
-		ClientBase aBase = new ClientBase();
-		DriverBase anotherBase = new DriverBase();
 		Matrix theMatrix = new Matrix(aBase, anotherBase);
-		Fiat600 aCar = new Fiat600();
-		Driver aDriver = new Driver("mario", 23423, "holass", aCar);
 		theMatrix.addDriver(aDriver);
 		assertTrue(theMatrix.checkDriverPassword(aDriver.getName(), "holass"));
 	}
 	@Test
 	public void  checkAskForCarTest() {
-		try {
-			bot = new Robot();
-		} catch (AWTException e) {
-			e.printStackTrace();
-		}
-		Thread ok = new Thread () {
-			  public void run () {
-				  try{Thread.sleep(250);}catch(InterruptedException e){}
-				  bot.keyPress(KeyEvent.VK_ENTER);
-					try{Thread.sleep(50);}catch(InterruptedException e){}
-					bot.keyRelease(KeyEvent.VK_ENTER);
-			  }
-			};
-		ClientBase aBase = new ClientBase();
-		DriverBase anotherBase = new DriverBase();
+		try { bot = new Robot(); } catch (AWTException e) {}
 		Matrix theMatrix = new Matrix(aBase, anotherBase);
-		User aUser = new User("Mario", 1231, "holaaaaaa");
 		theMatrix.addUser(aUser);
 		theMatrix.addMoney(aUser, 234);
-		Fiat600 aCar = new Fiat600();
-		Driver aDriver = new Driver("mario", 23423, "holass", aCar);
 		theMatrix.addDriver(aDriver);
 		aDriver.goOnline();
 		long[] aDestination = {23, 3};
