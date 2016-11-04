102a5ef39593ebce8b21cf0dd7960c94da48280a
nanogila <nanogi69@gmail.com>
2016-11-03 23:48:12 -0300

    ok, travis is slow

--- rUBERn/test/MatrixTester.java
+++ rUBERn/test/MatrixTester.java
@@ -25,9 +25,7 @@ public class MatrixTester {
 			  public void run () {
 					try { bot = new Robot(); } catch (AWTException e) {}
 				  for (int i = 0; i<10; i++) {
-					  
-					  
-					try{Thread.sleep(200);}catch(InterruptedException e){}
+					  try{Thread.sleep(500);}catch(InterruptedException e){}
 				  bot.keyPress(KeyEvent.VK_ENTER);
 					try{Thread.sleep(10);}catch(InterruptedException e){}
 					bot.keyRelease(KeyEvent.VK_ENTER);
